cb0f0ba11edbe1e701423d4aee20cfb4514ad712
inveker <alex.korolev.1995@gmail.com>
2025-02-18 14:29:16 +0300

    feat(signer-service): multisig

--- services/signer-service/src/types/queue.types.ts
+++ services/signer-service/src/types/queue.types.ts
@@ -2,12 +2,27 @@ export interface UnsignedMessage {
   id: string;
   data: string;     // Hex string to sign
   chainId: number;  // EVM chain ID
+  requiredSignatures: number; // Number of signatures required
+  totalSigners: number;      // Total number of available signers
+}
+
+export interface Signature {
+  signature: string;
+  signer: string;
+}
+
+export interface CollectingMessage {
+  id: string;
+  data: string;     // Original data
+  chainId: number;  // EVM chain ID
+  requiredSignatures: number;
+  totalSigners: number;
+  signatures: Signature[];
 }
 
 export interface SignedMessage {
   id: string;
   data: string;     // Original data
-  signature: string; // Signed data
-  signer: string;   // Address that signed
   chainId: number;  // EVM chain ID
+  signatures: Signature[];
 }
